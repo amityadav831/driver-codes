2f7ec259837e45ed83a71ca3575f827a97246d45
amityadav831 <83361759+amityadav831@users.noreply.github.com>
2022-04-08 12:46:33 +0530

    Update java-driver-code.java

--- numbers/java-driver-code.java
+++ numbers/java-driver-code.java
@@ -1,28 +1,24 @@
-import java.io.*;
-import java.util.*;
-
-public class Main {
-  	// The main function reads the input in the required format.
-  	// Just complete the solve function below.
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter wr = new PrintWriter(System.out);
-        int T = Integer.parseInt(br.readLine().trim());
-        for(int t_i = 0; t_i < T; t_i++)
-        {
-            int n =  Integer.parseInt(br.readLine());
-            String out_ = solve(n);
-            System.out.println(out_);
-         }
-         wr.close();
-         br.close();
+class Magic
+{
+  public static boolean magicNumber(int n)
+  {
+    int sum = 0;
+    while(n>0 || sum>9)
+    {
+      if(n == 0)
+      {
+        n = sum;
+        sum = 0;
+      }
+      return(sum == 1);
     }
-  
-    static String solve(int n){
-      	// Your code goes here
-        // n is the given input
-      
-        String result = "Special";
-        return result;
+    public static void main(String args[])
+    {
+      int n = 132;
+      if(magicNumber(n))
+        System.out.println("Magic Number");
+      else
+        System.out.println("Normal Number");
     }
+  }
 }
